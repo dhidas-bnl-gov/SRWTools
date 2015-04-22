c39b0ef0eb641dd1a493fbf13bcc6a7b95a288ce
Dean Andrew Hidas <dhidas@130-199-141-189.dhcp.bnl.gov>
2015-04-22 17:28:08 -0400

    simulated and field from data now in same script

--- python/UndulatorRadiation.py
+++ python/UndulatorRadiation.py
@@ -9,7 +9,13 @@ import matplotlib.pyplot as plt
 
 
 # grab the input date file from the command line
-InFileName = sys.argv[1]
+InFileName = ""
+if len(sys.argv) == 1:
+  InFileName = sys.argv[1]
+elif len(sys.argv) > 1:
+  print 'Usage: ', sys.argv[0], '[InFileName]'
+  print 'If InFileName not specified, will run for simulated undulator'
+  exit(0)
 
 
 
@@ -18,22 +24,41 @@ magFldCnt = SRWLMagFldC()
 magFldCnt.allocate(1)
 
 
-# Read data from file and make mag field object
-magFldCnt.arMagFld[0] = ReadHallProbeDataSRW(InFileName)
 
-# Field interpolation method
-magFldCnt.arMagFld[0].interp = 4
+if False:
+  # define the undulator specs
+  numPer = 70
+  undPer = 0.021
+  Bx = 0.0
+  By = 1.2
+  phBx = 0
+  phBy = 0
+  sBx = 1
+  sBy = 1
+  xcID = 0
+  ycID = 0
+  zcID = 0
 
-# ID center
-magFldCnt.arXc[0] = 0.0
-magFldCnt.arYc[0] = 0.0
+  und = SRWLMagFldU([SRWLMagFldH(1, 'v', By, phBy, sBy, 1), SRWLMagFldH(1, 'h', Bx, phBx, sBx, 1)], undPer, numPer)
+  magFldCnt = SRWLMagFldC([und], array('d', [xcID]), array('d', [ycID]), array('d', [zcID])) #Container of all Field Elements
+else :
+  # Read data from file and make mag field object
+  magFldCnt.arMagFld[0] = ReadHallProbeDataSRW(InFileName)
+
+  # Field interpolation method
+  magFldCnt.arMagFld[0].interp = 4
+
+  # ID center
+  magFldCnt.arXc[0] = 0.0
+  magFldCnt.arYc[0] = 0.0
 
 
-# Number of reps of field
-magFldCnt.arMagFld[0].nRep = 1
+  # Number of reps of field
+  magFldCnt.arMagFld[0].nRep = 1
 
-# Center in Z of ID
-magFldCnt.arZc[0] = 0.0
+  # Center in Z of ID
+  magFldCnt.arZc[0] = 0.0
+
 
 
 # Electron Beam
@@ -58,23 +83,24 @@ wfr1.partBeam = elecBeam
 
 
 
-#***********Precision
-meth = 1 #SR calculation method: 0- "manual", 1- "auto-undulator", 2- "auto-wiggler"
-relPrec = 0.01 #relative precision
-zStartInteg = 0 #longitudinal position to start integration (effective if < zEndInteg)
-zEndInteg = 0 #longitudinal position to finish integration (effective if > zStartInteg)
-npTraj = 20000 #Number of points for trajectory calculation 
-useTermin = 1 #Use "terminating terms" (i.e. asymptotic expansions at zStartInteg and zEndInteg) or not (1 or 0 respectively)
-sampFactNxNyForProp = 0 #sampling factor for adjusting nx, ny (effective if > 0)
+# Precision
+meth = 1
+relPrec = 0.01
+zStartInteg = 0
+zEndInteg = 0
+npTraj = 20000
+useTermin = 1
+sampFactNxNyForProp = 0
 arPrecPar = [meth, relPrec, zStartInteg, zEndInteg, npTraj, useTermin, sampFactNxNyForProp]
 
-#**********************Calculation (SRWLIB function calls)
+# Calculation (SRWLIB function calls)
 srwl.CalcElecFieldSR(wfr1, 0, magFldCnt, arPrecPar)
 arI1 = array('f', [0]*wfr1.mesh.ne)
 srwl.CalcIntFromElecField(arI1, wfr1, 6, 0, 0, wfr1.mesh.eStart, wfr1.mesh.xStart, wfr1.mesh.yStart)
 
-
-print [wfr1.mesh.eStart, wfr1.mesh.eFin, wfr1.mesh.ne]
+# Get X-values for plotting
 XValues = [(wfr1.mesh.eStart + float(x) * (wfr1.mesh.eFin - wfr1.mesh.eStart) / wfr1.mesh.ne) for x in range(wfr1.mesh.ne)]
+
+# Make some plots
 plt.plot(XValues, arI1)
 plt.show()
