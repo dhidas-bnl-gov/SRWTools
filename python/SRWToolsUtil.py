61f15fdd98e5e9d54245063ba4cbbf45c8073d0f
Dean Andrew Hidas <dhidas@LPS-156278.local>
2015-05-04 12:20:15 -0400

    additions for finding max and mins

--- python/SRWToolsUtil.py
+++ python/SRWToolsUtil.py
@@ -1,9 +1,10 @@
 # Path to SRW python lib and import as srw
 import sys
-#sys.path.append('/home/dhidas/SRW/env/work/srw_python')
+sys.path.append('/home/dhidas/SRW/env/work/srw_python')
 sys.path.append('/Users/dhidas/SRW/env/work/srw_python')
 from srwlib import *
 
+from ROOT import TFile, TTree, TGraph, TCanvas, TF1
 
 
 def ReadHallProbeData (InFileName, ZMin = -999, ZMax = 999):
@@ -93,3 +94,92 @@ def IntegralVector (X, Y):
     I.append(I[-1:][0] + trap)
 
   return I
+
+
+
+def FindMaxAndMins (Z, By):
+  "find the max and mins in the By distribution"
+
+  # Loop over data and find min/max for every point
+  IsAbove = False
+  IsBelow = False
+  MaxBy = 0
+  MinBy = 0
+  ZMax  = 0
+  ZMin  = 0
+  ZMaxI = 0
+  ZMinI = 0
+  MaxListInd  = []
+  MaxListBy   = []
+
+  BThreshold = 0.0004
+  for i in range( len(Z) ):
+    if By[i] < -BThreshold:
+      if IsAbove:
+        MaxListInd.append(ZMaxI)
+        MaxListBy.append(MaxBy)
+        MaxBy = 0
+      IsBelow = True
+      IsAbove = False
+    if By[i] > BThreshold:
+      if IsBelow:
+        MaxListInd.append(ZMinI)
+        MaxListBy.append(MinBy)
+        MinBy = 0
+      IsBelow = False
+      IsAbove = True
+
+    if IsAbove and By[i] > MaxBy:
+      MaxBy = By[i]
+      ZMax  = Z[i]
+      ZMaxI = i
+    if IsBelow and By[i] < MinBy:
+      MinBy = By[i]
+      ZMin  = Z[i]
+      ZMinI = i
+  if IsAbove:
+    MaxListInd.append(ZMaxI)
+    MaxListBy.append(MaxBy)
+  if IsBelow:
+    MaxListInd.append(ZMinI)
+    MaxListBy.append(MinBy)
+
+  print 'Number of max/min seen I: ', len(MaxListInd)
+
+  return [MaxListInd, MaxListBy]
+
+
+
+
+def FindMinMaxFromFit (MaxListInd, Z, By):
+  "Return the max B and Z locations from a fit"
+
+  # Where is the calculated max By from the fit
+  MaxBy  = []
+  MaxByZ = []
+
+  # Calculate the max based on pol2 fit maximum.  Assumption is to use max +-NFitWidth
+  NFitWidth = 1
+  for i in MaxListInd:
+    x = []
+    y = []
+
+    for j in range(-NFitWidth, NFitWidth + 1):
+      x.append(Z[i+j])
+      if By[i] >= 0:
+        y.append(By[i+j])
+      else:
+        y.append(-By[i+j])
+
+    g = TGraph(NFitWidth*2 + 1, array('d', x), array('d', y))
+    g.SetName('gFit' + str(i))
+    FitFunction = TF1("FitFunction_"+str(i), "pol2", -4, 4)
+    g.Fit(FitFunction, 'q')
+    g.Write()
+    MaxByZ.append(FitFunction.GetMaximumX())
+    if By[i] >= 0:
+      MaxBy.append(FitFunction.GetMaximum())
+    else:
+      MaxBy.append(-FitFunction.GetMaximum())
+
+  return [MaxByZ, MaxBy]
