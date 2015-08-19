fb40c354582a89d2981f266f47afe097b05da4ad
Hidas <dhidas@id01.ags.bnl.gov>
2015-08-19 10:46:26 -0400

    comment out data spectrum for now

--- python/SRWToolsUtil.py
+++ python/SRWToolsUtil.py
@@ -236,10 +236,10 @@ def GetUndulatorSpectrum (magFldCnt, elecBeam):
   wfr1.mesh.zStart = 20.
   wfr1.mesh.eStart = 10.
   wfr1.mesh.eFin = 70000.
-  wfr1.mesh.xStart = -0.000001
-  wfr1.mesh.xFin = 0.000001
-  wfr1.mesh.yStart = -0.000001
-  wfr1.mesh.yFin = 0.000001
+  wfr1.mesh.xStart = 0.0
+  wfr1.mesh.xFin = 0.0
+  wfr1.mesh.yStart = 0.0
+  wfr1.mesh.yFin = 0.0
   wfr1.partBeam = elecBeam
 
 
