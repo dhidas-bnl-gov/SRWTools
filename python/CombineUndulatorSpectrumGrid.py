0b01b929e6a2d2b73128a7b879287947833ba3a5
dhidas-bnl-gov <dhidas@bnl.gov>
2015-08-27 08:57:43 -0400

    functionality for combination of grid spectrum results

---
+++ python/CombineUndulatorSpectrumGrid.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+
+
+N = len(sys.argv) - 1
+print 'N files:', N
+
+
+
+Seeds = []
+Values = []
+
+for i in range(1, N + 1):
+  FileName = sys.argv[i]
+  print 'Adding:', FileName
+
+  fi = open(FileName, 'r')
+
+  Seed = int(fi.readline())
+
+  # Check for seeing this seed
+
+  j = 0
+  for l in fi:
+    l = l.strip()
+    [x, y] = map(float, l.split())
+
+    if i == 1:
+      Values.append([x, y / N])
+    else:
+      Values[j][1] += y / N
+
+    j += 1
+
+
+#BaseFileName = os.path.splitext(os.path.basename(InFileName))[0]
+#OutFileName = BaseFileName + '_Average.dat'
+OutFileName = 'Average.dat'
+fo = open(OutFileName, 'w')
+fo.write('0\n')
+for v in Values:
+  fo.write(str(v[0]) + ' ' + str(v[1]) + '\n')
+fo.close()
